d6af2be54b0324b16e1c0efefee8f4a5e09abdd6
weekysui <weekysui@hotmail.com>
2018-05-09 15:11:30 -0700

    completed

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask,render_template,jsonify
+import sqlalchemy
+from sqlalchemy import create_engine,inspect,desc
+from sqlalchemy.ext.automap import automap_base
+Base = automap_base()
+engine = create_engine('sqlite:///dataSets/belly_button_biodiversity.sqlite')
+Base.prepare(engine, reflect = True)
+from sqlalchemy.orm import Session
+session = Session(engine)
+Otu = Base.classes.otu
+Samples = Base.classes.samples
+SamplesMetadata = Base.classes.samples_metadata
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/names")
+def names():
+    inspector = inspect(engine)
+    columns = inspector.get_columns("samples")
+    names = []
+    for column in columns:
+        names.append(column["name"])
+    names.pop(0)
+    return jsonify(names)
+@app.route("/otu")
+def otu():
+    otu_description = session.query(Otu.lowest_taxonomic_unit_found).all()
+    otu_description=[x[0] for x in otu_description]
+    return jsonify(otu_description)
+@app.route("/otu/<otu_id>")
+def otuSample(otu_id):
+    results = session.query(Otu.lowest_taxonomic_unit_found).filter(Otu.otu_id==otu_id).all()
+    return jsonify(results)
+@app.route("/metadata/<sample>")
+def metaSample(sample):
+    sample=sample.replace("BB_","")
+    results = session.query(
+        SamplesMetadata.AGE,
+        SamplesMetadata.BBTYPE,
+        SamplesMetadata.ETHNICITY,
+        SamplesMetadata.GENDER,
+        SamplesMetadata.LOCATION,
+        SamplesMetadata.SAMPLEID).filter(SamplesMetadata.SAMPLEID==sample).all()
+    result = results[0]
+    result_dict = {}
+    result_dict["AGE"]=result[0]
+    result_dict["BBTYPE"]=result[1]
+    result_dict["ETHNICITY"]=result[2]
+    result_dict["GENDER"]=result[3]
+    result_dict["LOCATION"]=result[4]
+    result_dict["SAMPLEID"]=result[5]
+    return jsonify(result_dict)
+@app.route("/wfreq/<sample>")
+def wfreq(sample):
+    sample = sample.replace("BB_","")
+    results = session.query(SamplesMetadata.WFREQ).filter(SamplesMetadata.SAMPLEID==sample).all()
+    return jsonify(results[0][0])
+@app.route("/samples/<sample>")
+def sampleValues(sample):
+    query="Samples."+sample
+    results = session.query(Samples.otu_id,query).order_by(desc(query)).all()
+    otu_ids = []
+    sample_values = []
+    for x in range(len(results)):
+        otu_ids.append(results[x][0])
+        sample_values.append(results[x][1])
+    otu_dict_list = [{"otu_ids":otu_ids,"sample_values":sample_values}]
+    return jsonify(otu_dict_list)
+if __name__=="__main__":
+    app.run(debug=True)
---
+++ static/js/index.js
@@ -0,0 +1,149 @@
+// create dropdown values
+
+d3.json("/names",function(error,response){
+    if(error) throw error;
+    var dropdown = document.getElementById("selDataset");
+    for (var i = 0,ii=response.length;i<ii;i++){
+        var $option = document.createElement("option");
+        $option.innerText=response[i];
+        $option.setAttribute("value",response[i])
+        dropdown.appendChild($option);
+    };
+});
+
+// initial value
+defaultSample = "BB_940"
+function init(sample){
+    var metaUrl = `/metadata/${sample}`;
+    d3.json(metaUrl,function(error,response){
+        if(error) throw error;
+        var sampleData = d3.select("#metadataSample")
+        sampleData.append("p").text(`AGE: ${response.AGE}`)
+        sampleData.append("p").text(`BBTYPE: ${response.BBTYPE}`)
+        sampleData.append("p").text(`ETHNICITY: ${response.ETHNICITY}`)
+        sampleData.append("p").text(`GENDER: ${response.GENDER}`)
+        sampleData.append("p").text(`LOCATION: ${response.LOCATION}`)
+        sampleData.append("p").text(`SAMPLEID: ${response.SAMPLEID}`)
+    })
+    var url = `/samples/${sample}`;
+    console.log(url);
+    d3.json(url,function(error,response){
+        if(error) throw error;
+        var values = response[0]["sample_values"].slice(0,10);
+        var labels = response[0]["otu_ids"].slice(0,10);
+        console.log(values)
+    
+        var $hoverText = []
+        
+        for(var i = 0, ii=labels.length;i<ii;i++){
+            var query_url = `/otu/${labels[i]}`
+            d3.json(query_url,function(error,otuResponse){
+                if(error) throw error;
+                $hoverText.push(otuResponse[0])
+            })
+        }
+        console.log($hoverText)
+        
+        var data1 = [{
+            "values":values,
+            "labels":labels,
+            "type":"pie",
+            "text":$hoverText
+        }]
+
+        Plotly.newPlot("pie",data1)
+        var data2 = [{
+            "x":response[0]["otu_ids"],
+            "y":response[0]["sample_values"],
+            "mode":"markers",
+            "type":"scatter",
+            "marker":{
+                "size":response[0]["sample_values"],
+                "color":response[0]["otu_ids"]
+            },
+            "hovertext":$hoverText,
+            "hoverinfo": {bordercolor: 'black'}
+        }];
+        var layout = {
+            xaxis:{
+                title:"OTU ID"
+            },
+            yaxis:{
+                title:"Sample Values"
+            }
+        }
+        Plotly.newPlot("bubble_chart",data2,layout)  
+    })
+    freq_url = `/wfreq/${sample}`
+    d3.json(freq_url,function(error,freqResponse){
+        if(error) throw error;
+        var level = freqResponse*20;
+        var degree = 180-level,
+            radius = .5;
+        var radians = degree * Math.PI / 180;
+        var x = radius * Math.cos(radians);
+        var y = radius * Math.sin(radians);
+        var mainPath = "M -.0 -0.025 L .0 0.025 L",
+            pathX = String(x),
+            space = ' ',
+            pathY = String(y),
+            pathEnd = ' Z';
+        var path = mainPath.concat(pathX,space,pathY,pathEnd);
+        var data3 = [{ type: 'scatter',
+            x: [0], y:[0],
+            marker: {size: 15, color:'850000'},
+            showlegend: false,
+            // name: 'Number of Washes',
+            // text: level,
+            hoverinfo: 'name'},
+            {values: [50/5, 50/5, 50/5, 50/5, 50/5, 50],
+            rotation: 90,
+            text:['8-9', '6-7', '4-5', '2-3','0-1', " "],
+            textinfo:"text",
+            textposition:"inside",
+            marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+            'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+            'rgba(210, 206, 145, .5)', 'white',
+            'rgba( 0, 0, 0, 0.8 )']},
+            labels: ['8-9', '6-7', '4-5', '2-3','0-1', " "],
+            hoverinfo:'label',
+            hole:.5,
+            type:'pie',
+            showlegend:false
+        }];
+        var layout = {
+            shapes:[{
+                type: 'path',
+                path: path,
+                fillcolor: '850000',
+                line: {
+                    color: '850000'
+                },
+            }],
+            title:"<b>Belly Button Washing Frequency</b><br>Washes per Week",
+            height: 450,
+            margin: {
+                top: 50,
+                bottom: 10,
+                right: 10,
+                left: 10
+            },
+            xaxis: {zeroline:false, showticklabels:false,
+                        showgrid: false, range: [-1, 1]},
+            yaxis: {zeroline:false, showticklabels:false,
+                        showgrid: false, range: [-1, 1]}
+            };
+            
+        
+        Plotly.newPlot("gauge",data3,layout)
+    })
+}
+init(defaultSample);
+function getData(new_samples){
+    d3.select("#metadataSample").selectAll('p').remove();
+    init(new_samples)
+}
+
+
+
+
